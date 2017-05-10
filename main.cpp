ebd746397b00b0f838e6b64148ab59c7b2f9608c
Maciej Stępień <maciek1284@outlook.com>
2017-05-10 18:46:24 +0200

    commit żeby pushnac

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main()
 
     Robot rob(Point3f(0, 13 ,100), Point3f(0,0,0), 11.8, 36.5, Point3f(3.7, 5.8, 16.3));
 
-    float transStep = 5;
+    float transStep = 1;
     float rotStep = 0.05;
 
     while(key != 27)
