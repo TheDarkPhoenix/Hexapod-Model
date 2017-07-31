6e22f14c2c4b492af7a00e8f517dcd8c7068fd90
Maciej Stępień <maciek1284@outlook.com>
2017-07-31 20:34:53 +0200

    zmiany w układach odniesienia podczas obliczeń

--- robot.cpp
+++ robot.cpp
@@ -16,66 +16,49 @@ Robot::Robot(cv::Point3f pos, cv::Point3f ang, float width1, float length1, cv::
     initPosition = position;
     initAngles = angles;
     walkingStep = 0;
-    /*Rx = (Mat_<float>(3,3) << 1, 0, 0, 0, cos(angles.x), -sin(angles.x), 0, sin(angles.x), cos(angles.x));
-    Ry = (Mat_<float>(3,3) << cos(angles.y), 0, sin(angles.y), 0, 1, 0, -sin(angles.y), 0, cos(angles.y));
-    Rz = (Mat_<float>(3,3) << cos(angles.z), -sin(angles.z), 0, sin(angles.z), cos(angles.z), 0, 0, 0, 1);
-    R = Rz*Ry*Rx;
-    Mat P1 = (Mat_<float>(3,1) << position.x - width/2, -position.y, position.z - length/2);
-    Mat P2 = (Mat_<float>(3,1) << position.x + width/2, -position.y, position.z - length/2);
-    Mat P3 = (Mat_<float>(3,1) << position.x - width/2, -position.y, position.z + length/2);
-    Mat P4 = (Mat_<float>(3,1) << position.x + width/2, -position.y, position.z + length/2);
-    Mat P11 = R*P1;
-    Mat P22 = R*P2;
-    Mat P33 = R*P3;
-    Mat P44 = R*P4;
-    frame.dl = Point3f(P11.at<float>(0,0), P11.at<float>(0,1), P11.at<float>(0,2));
-    frame.dr = Point3f(P22.at<float>(0,0), P22.at<float>(0,1), P22.at<float>(0,2));
-    frame.ul = Point3f(P33.at<float>(0,0), P33.at<float>(0,1), P33.at<float>(0,2));
-    frame.ur = Point3f(P44.at<float>(0,0), P44.at<float>(0,1), P44.at<float>(0,2));*/
 
-    update();
-    Mat Ryy = (Mat_<float>(3,3) << -1, 0, 0, 0, 1, 0, 0, 0, -1);
-
+    lFrame.dl = Point3f(-width/2,0,-length/2);
+    lFrame.dr = Point3f(width/2,0,-length/2);
+    lFrame.ul = Point3f(-width/2,0,length/2);
+    lFrame.ur = Point3f(width/2,0,length/2);
 
     for(int i = 3; i < 6; ++i)
-        legs[i].setR(Rz*Ryy*Ry*Rx);
+        legs[i].setR((Mat_<float>(3,3) << -1, 0, 0, 0, 1, 0, 0, 0, -1));
     for(int i = 0; i < 3; ++i)
-        legs[i].setR(R);
+        legs[i].setR((Mat_<float>(3,3) << 1, 0, 0, 0, 1, 0, 0, 0, 1));
 
-    //Point3f ang1(0. ,-0.3 ,1.2);
-    //Point3f ang1(0. ,-0.1 ,1.5);
     Point3f ang1(0.0 ,0.0 ,CV_PI/2);
-    legs[0].setJointA(frame.ur);
+    legs[0].setJointA(lFrame.ur);
     legs[0].setAgnles(ang1);
     legs[0].setLengths(leglengths);
     legs[0].setSignals(Point3f(5900,5100,5200));
     legs[0].setServos(Point3i(5,4,3));
 
-    legs[1].setJointA((frame.ur+frame.dr)/2);
+    legs[1].setJointA((lFrame.ur+lFrame.dr)/2);
     legs[1].setAgnles(ang1);
     legs[1].setLengths(leglengths);
     legs[1].setSignals(Point3f(6000,5200,4900));
     legs[1].setServos(Point3i(9,10,11));
 
-    legs[2].setJointA(frame.dr);
+    legs[2].setJointA(lFrame.dr);
     legs[2].setAgnles(ang1);
     legs[2].setLengths(leglengths);
     legs[2].setSignals(Point3f(6200,5300,5300));
     legs[2].setServos(Point3i(15,16,17));
 
-    legs[3].setJointA(frame.ul);
+    legs[3].setJointA(lFrame.ul);
     legs[3].setAgnles(ang1);
     legs[3].setLengths(leglengths);
     legs[3].setSignals(Point3f(6000,5300,4900));
     legs[3].setServos(Point3i(0,1,2));
 
-    legs[4].setJointA((frame.ul+frame.dl)/2);
+    legs[4].setJointA((lFrame.ul+lFrame.dl)/2);
     legs[4].setAgnles(ang1);
     legs[4].setLengths(leglengths);
     legs[4].setSignals(Point3f(6000,5000,5000));
     legs[4].setServos(Point3i(6,7,8));
 
-    legs[5].setJointA(frame.dl);
+    legs[5].setJointA(lFrame.dl);
     legs[5].setAgnles(ang1);
     legs[5].setLengths(leglengths);
     legs[5].setSignals(Point3f(6000,5300,5200));
@@ -86,52 +69,46 @@ Robot::Robot(cv::Point3f pos, cv::Point3f ang, float width1, float length1, cv::
         legs[i].initJointPoints();
         legs[i].setDevice(&device);
     }
-    stringstream ss;
-    for(int i = 0; i < 18; ++i)
-    {
-        ss << "usccmd --speed "<<(i)<<","<<(100);
-        string str = ss.str();
-        //system(str.c_str());
-        ss.clear();
-    }
-    /*ss << "usccmd --speed "<<(0)<<","<<(10);
-    string str = ss.str();
-    system(str.c_str());
-    ss.clear();
-
-    ss << "usccmd --speed "<<(1)<<","<<(10);
-     str = ss.str();
-    system(str.c_str());
-    ss.clear();
-
-    ss << "usccmd --speed "<<(2)<<","<<(10);
-     str = ss.str();
-    system(str.c_str());
-    ss.clear();*/
 }
 
 joints Robot::getLegJoints(int n)
 {
     if(!(n>=0 && n<=5))
-        return joint();
+        return joints();
 
+    joints x = legs[n].getJoints();
 
-
-    return legs[n].getJoints();
-}
-
-void Robot::update()
-{
     Rx = (Mat_<float>(3,3) << 1, 0, 0, 0, cos(angles.x), -sin(angles.x), 0, sin(angles.x), cos(angles.x));
     Ry = (Mat_<float>(3,3) << cos(angles.y), 0, sin(angles.y), 0, 1, 0, -sin(angles.y), 0, cos(angles.y));
     Rz = (Mat_<float>(3,3) << cos(angles.z), -sin(angles.z), 0, sin(angles.z), cos(angles.z), 0, 0, 0, 1);
 
     R = Rz*Ry*Rx;
 
-    /*Mat P1 = (Mat_<float>(3,1) << position.x - width/2, -position.y, position.z - length/2);
-    Mat P2 = (Mat_<float>(3,1) << position.x + width/2, -position.y, position.z - length/2);
-    Mat P3 = (Mat_<float>(3,1) << position.x - width/2, -position.y, position.z + length/2);
-    Mat P4 = (Mat_<float>(3,1) << position.x + width/2, -position.y, position.z + length/2);*/
+    Mat P1 = (Mat_<float>(3,1) << x.A.x, x.A.y, x.A.z);
+    Mat P2 = (Mat_<float>(3,1) << x.B.x, x.B.y, x.B.z);
+    Mat P3 = (Mat_<float>(3,1) << x.C.x, x.C.y, x.C.z);
+    Mat P4 = (Mat_<float>(3,1) << x.D.x, x.D.y, x.D.z);
+
+    Mat P11 = R*P1;
+    Mat P22 = R*P2;
+    Mat P33 = R*P3;
+    Mat P44 = R*P4;
+
+    x.A = Point3f(P11.at<float>(0,0), P11.at<float>(0,1), P11.at<float>(0,2)) + position;
+    x.B = Point3f(P22.at<float>(0,0), P22.at<float>(0,1), P22.at<float>(0,2)) + position;
+    x.C = Point3f(P33.at<float>(0,0), P33.at<float>(0,1), P33.at<float>(0,2)) + position;
+    x.D = Point3f(P44.at<float>(0,0), P44.at<float>(0,1), P44.at<float>(0,2)) + position;
+
+    return x;
+}
+
+rect Robot::getFrame()
+{
+    Rx = (Mat_<float>(3,3) << 1, 0, 0, 0, cos(angles.x), -sin(angles.x), 0, sin(angles.x), cos(angles.x));
+    Ry = (Mat_<float>(3,3) << cos(angles.y), 0, sin(angles.y), 0, 1, 0, -sin(angles.y), 0, cos(angles.y));
+    Rz = (Mat_<float>(3,3) << cos(angles.z), -sin(angles.z), 0, sin(angles.z), cos(angles.z), 0, 0, 0, 1);
+
+    R = Rz*Ry*Rx;
 
     Mat P1 = (Mat_<float>(3,1) << - width/2, 0, - length/2);
     Mat P2 = (Mat_<float>(3,1) << width/2, 0, - length/2);
@@ -143,65 +120,65 @@ void Robot::update()
     Mat P33 = R*P3;
     Mat P44 = R*P4;
 
-    frame.dl = Point3f(P11.at<float>(0,0), P11.at<float>(0,1), P11.at<float>(0,2)) + position;
-    frame.dr = Point3f(P22.at<float>(0,0), P22.at<float>(0,1), P22.at<float>(0,2)) + position;
-    frame.ul = Point3f(P33.at<float>(0,0), P33.at<float>(0,1), P33.at<float>(0,2)) + position;
-    frame.ur = Point3f(P44.at<float>(0,0), P44.at<float>(0,1), P44.at<float>(0,2)) + position;
+    gFrame.dl = Point3f(P11.at<float>(0,0), P11.at<float>(0,1), P11.at<float>(0,2)) + position;
+    gFrame.dr = Point3f(P22.at<float>(0,0), P22.at<float>(0,1), P22.at<float>(0,2)) + position;
+    gFrame.ul = Point3f(P33.at<float>(0,0), P33.at<float>(0,1), P33.at<float>(0,2)) + position;
+    gFrame.ur = Point3f(P44.at<float>(0,0), P44.at<float>(0,1), P44.at<float>(0,2)) + position;
 
-    Mat Ryy = (Mat_<float>(3,3) << -1, 0, 0, 0, 1, 0, 0, 0, -1);
+    return gFrame;
+}
 
-    for(int i = 3; i < 6; ++i)
-        legs[i].setR(Rz*Ryy*Ry*Rx);
+void Robot::moveCoordinates(Point3f p, Point3f ang)
+{
+    angles += ang;
 
-    for(int i = 0; i < 3; ++i)
-        legs[i].setR(R);
+    ang = -ang;
 
-    legs[0].setJointA(frame.ur);
-    legs[1].setJointA((frame.ur+frame.dr)/2);
-    legs[2].setJointA(frame.dr);
-    legs[3].setJointA(frame.ul);
-    legs[4].setJointA((frame.ul+frame.dl)/2);
-    legs[5].setJointA(frame.dl);
+
+    Rx = (Mat_<float>(3,3) << 1, 0, 0, 0, cos(ang.x), -sin(ang.x), 0, sin(ang.x), cos(ang.x));
+    Ry = (Mat_<float>(3,3) << cos(ang.y), 0, sin(ang.y), 0, 1, 0, -sin(ang.y), 0, cos(ang.y));
+    Rz = (Mat_<float>(3,3) << cos(ang.z), -sin(ang.z), 0, sin(ang.z), cos(ang.z), 0, 0, 0, 1);
+
+    R = Rz*Ry*Rx;
+
+    joints x;
+
+    Mat P1, P11;
+
+    for(int i = 0; i < 6; ++i)
+    {
+        x = legs[i].getJoints();
+        P1 = (Mat_<float>(3,1) << x.D.x, x.D.y, x.D.z);
+        P11 = R*P1;
+        x.D = Point3f(P11.at<float>(0,0), P11.at<float>(0,1), P11.at<float>(0,2)) - p;
+        legs[i].setLegEnd(x.D);
+        legs[i].calculateAngles(angles);
+    }
+    Rx = (Mat_<float>(3,3) << 1, 0, 0, 0, cos(angles.x), -sin(angles.x), 0, sin(angles.x), cos(angles.x));
+    Ry = (Mat_<float>(3,3) << cos(angles.y), 0, sin(angles.y), 0, 1, 0, -sin(angles.y), 0, cos(angles.y));
+    Rz = (Mat_<float>(3,3) << cos(angles.z), -sin(angles.z), 0, sin(angles.z), cos(angles.z), 0, 0, 0, 1);
+
+    R = Rz*Ry*Rx;
+    P1 = (Mat_<float>(3,1) << p.x, p.y, p.z);
+    P11 = R*P1;
+    p = Point3f(P11.at<float>(0,0), P11.at<float>(0,1), P11.at<float>(0,2));
+    position += p;
 }
 
 void Robot::move(Point3f p)
 {
+    moveCoordinates(p, Point3f(0,0,0));
 
-    position += p;
-
-    update();
-
-    if(legs[0].calculateAngles(-angles) == -1)
-            this->move(-p);
-    if(legs[4].calculateAngles(-angles) == -1)
-            this->move(-p);
-    if(legs[2].calculateAngles(-angles) == -1)
-            this->move(-p);
-    if(legs[5].calculateAngles(-angles) == -1)
-            this->move(-p);
-    if(legs[1].calculateAngles(-angles) == -1)
-            this->move(-p);
-    if(legs[3].calculateAngles(-angles) == -1)
-            this->move(-p);
-
-
-    /*for(int i = 0; i < 3; ++i)
+    for(int i = 0; i < 6; ++i)
     {
         if(legs[i].calculateAngles(-angles) == -1)
             this->move(-p);
     }
-    for(int i = 3; i < 6; ++i)
-    {
-        if(legs[i].calculateAngles(angles) == -1)
-            this->move(-p);
-    }*/
 }
 
 void Robot::rotate(Point3f ang)
 {
-    angles += ang;
-
-    update();
+    moveCoordinates(Point3f(0,0,0), ang);
 
     for(int i = 0; i < 3; ++i)
     {
@@ -253,6 +230,10 @@ void Robot::walk(Point3f steps)
     else if(walkingStep == 2)
     {
         move(steps);
+        ++walkingStep;
+    }
+    else if(walkingStep == 3)
+    {
         steps.x/=2;
         steps.y = -2;
         steps.z /=2;
@@ -268,7 +249,7 @@ void Robot::walk(Point3f steps)
 
         ++walkingStep;
     }
-    else if(walkingStep == 3)
+    else if(walkingStep == 4)
     {
         steps.x /= 2;
         steps.y = 2;
--- robot.h
+++ robot.h
@@ -17,7 +17,7 @@ class Robot
         float width, length;
         cv::Mat Rx, Ry, Rz, R;
         Leg legs[6];
-        void update();
+        void moveCoordinates(cv::Point3f p, cv::Point3f ang);
         int walkingStep;
         Maestro device;
 
@@ -25,7 +25,7 @@ class Robot
         Robot(cv::Point3f pos, cv::Point3f ang, float width1, float length1, cv::Point3f leglengths);
         cv::Point3f getPosition(){return position;};
         cv::Point3f getAngles(){return angles;};
-        rect getFrame(){return frame;};
+        rect getFrame();
         joints getLegJoints(int n);
         void move(cv::Point3f p);
         void rotate(cv::Point3f ang);
