b724a476e9ac22033f01145727befee0b3cbe203
Maciej Stępień <maciek1284@outlook.com>
2017-08-01 12:16:23 +0200

    dodane tryby poruszania

--- main.cpp
+++ main.cpp
@@ -25,88 +25,128 @@ int main()
 
     float walkStep = 3;
 
+    ///Tryby:
+    ///1 - stanie w miejscu i ruch translacyjny
+    ///2 - stanie w miesjscu i obroty
+    ///3 - poruszanie siê manualne
+    ///4 - poruszanie siê automatyczne
+
+    int mode = 1;
+
     while(key != 27)
     {
-        switch(key)
+        if(mode == 1)
         {
-            case 'D':
-                if(walking)
-                    rob.walk(Point3f(walkStep,0,0));
-                else
+            switch(key)
+            {
+                case 'D':
                     rob.move(Point3f(transStep,0,0));
-                break;
-            case 'A':
-                if(walking)
-                    rob.walk(Point3f(-walkStep,0,0));
-                else
+                    break;
+                case 'A':
                     rob.move(Point3f(-transStep,0,0));
-                break;
-            case 'Q':
-                if(walking)
-                    rob.walkC(Point3f(0,0,walkStep), view1);
-                else
+                    break;
+                case 'Q':
                     rob.move(Point3f(0,0,transStep));
-                break;
-            case 'E':
-                if(walking)
-                    rob.walkC(Point3f(0,0,-walkStep), view1);
-                else
+                    break;
+                case 'E':
                     rob.move(Point3f(0,0,-transStep));
-                break;
-            case 'S':
-                //rob.move(Point3f(0,transStep,0));
-                rob.walkRotC(0.05, view1);
-                break;
-            case 'W':
-                rob.walkRot(-0.05);
-                //rob.move(Point3f(0,-transStep,0));
-                break;
-
-            case '4':
-                rob.rotate(Point3f(0,rotStep,0));
-                break;
-            case '6':
-                rob.rotate(Point3f(0,-rotStep,0));
-                break;
-            case '8':
-                rob.rotate(Point3f(rotStep,0,0));
-                break;
-            case '2':
-                rob.rotate(Point3f(-rotStep,0,0));
-                break;
-            case '7':
-                rob.rotate(Point3f(0,0,rotStep));
-                break;
-            case '9':
-                rob.rotate(Point3f(0,0,-rotStep));
-                break;
-            /*case '8':
-                rob.walk(Point3f(0,0,walkStep));
-                break;
-            case '2':
-                rob.walk(Point3f(0,0,-walkStep));
-                break;
-            case '4':
-                rob.walk(Point3f(-walkStep,0,0));
-                break;
-            case '6':
-                rob.walk(Point3f(walkStep,0,0));
-                break;
-
-            case '7':
-                rob.walk(Point3f(-walkStep,0,walkStep));
-                break;
-            case '3':
-                rob.walk(Point3f(walkStep,0,-walkStep));
-                break;
-            case '9':
-                rob.walk(Point3f(walkStep,0,walkStep));
-                break;
-            case '1':
-                rob.walk(Point3f(-walkStep,0,-walkStep));
-                break;*/
+                    break;
+                case 'S':
+                    rob.move(Point3f(0,transStep,0));
+                    break;
+                case 'W':
+                    rob.move(Point3f(0,-transStep,0));
+                    break;
+            }
+        }
+        else if(mode == 2)
+        {
+            switch(key)
+            {
+                case 'W':
+                    rob.rotate(Point3f(0,rotStep,0));
+                    break;
+                case 'S':
+                    rob.rotate(Point3f(0,-rotStep,0));
+                    break;
+                case 'A':
+                    rob.rotate(Point3f(rotStep,0,0));
+                    break;
+                case 'D':
+                    rob.rotate(Point3f(-rotStep,0,0));
+                    break;
+                case 'Q':
+                    rob.rotate(Point3f(0,0,rotStep));
+                    break;
+                case 'E':
+                    rob.rotate(Point3f(0,0,-rotStep));
+                    break;
+                }
+        }
+        else if(mode == 3)
+        {
+            switch(key)
+            {
+                case 'D':
+                    rob.walk(Point3f(walkStep,0,0));
+                    break;
+                case 'A':
+                    rob.walk(Point3f(-walkStep,0,0));
+                    break;
+                case 'W':
+                    rob.walk(Point3f(0,0,walkStep));
+                    break;
+                case 'S':
+                    rob.walk(Point3f(0,0,-walkStep));
+                    break;
+                case 'Q':
+                    rob.walkRot(0.05);
+                    break;
+                case 'E':
+                    rob.walkRot(-0.05);
+                    break;
+            }
+        }
+        else if(mode == 4)
+        {
+            switch(key)
+            {
+                case 'D':
+                    rob.walkC(Point3f(walkStep,0,0), view1);
+                    break;
+                case 'A':
+                    rob.walkC(Point3f(-walkStep,0,0), view1);
+                    break;
+                case 'W':
+                    rob.walkC(Point3f(0,0,walkStep), view1);
+                    break;
+                case 'S':
+                    rob.walkC(Point3f(0,0,-walkStep), view1);
+                    break;
+                case 'E':
+                    rob.walkRotC(0.05, view1);
+                    break;
+                case 'Q':
+                    rob.walkRotC(-0.05, view1);
+                    break;
+            }
         }
 
+        switch(key)
+        {
+            case '1':
+                mode = 1;
+                break;
+            case '2':
+                mode = 2;
+                break;
+            case '3':
+                mode = 3;
+                break;
+            case '4':
+                mode = 4;
+                break;
+        }
         view1.update(key, rob);
         key = waitKey(10);
     }
